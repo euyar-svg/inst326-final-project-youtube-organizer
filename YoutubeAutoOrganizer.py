090e5121a0b67687b401227e6d3d389cc497e6e2
Emre Uyar <euyar@terpmail.umd.edu>
2026-04-10 21:43:18 -0400

    Create YoutubeAutoOrganizer.py
    Co-Authored-By: Jason5hen <201392460+Jason5hen@users.noreply.github.com>

---
+++ YoutubeAutoOrganizer.py
@@ -0,0 +1,22 @@
+# Youtube Auto Organizer
+
+# This script automatically organizes your YouTube 
+# watch later playlist into smaller  playlists based on the meta data
+# It will operate with  a total of 6 functions:
+# Input Parser (Emre)
+# Transcript Fetcher (emre)
+# Data Sanititzer (Jason)
+# Ai Tagging (Jason)
+# Playlist Generator (Nizar)
+# Video Batch Adder (Nizar)
+
+def input_parser():
+    # This function will parse the input from the user
+    # which is the YouTube playlist URL
+    # It will return the playlist ID and the channel ID
+    pass
+
+def transcript_fetcher(playlist_id):
+    # This function will fetch the transcripts of the videos
+    # in the playlist and will return a list of transcripts
+    pass
